cc315d787f25cca664f48b18af3a41d95595a699
A_Rijenth <a_rijenth@laposte.net>
2024-02-08 17:00:15 +0100

    feat: persist sed user data (#39)

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { PERSIST, persistReducer, persistStore } from 'redux-persist';
 import authenticationReducer from '../state/authentication';
+import seederReducer from '../state/seeder';
 
 const persistConfig = {
     key: 'root',
@@ -10,6 +11,7 @@ const persistConfig = {
 
 const reducer = combineReducers({
     authentication: authenticationReducer,
+    seeder: seederReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, reducer);
