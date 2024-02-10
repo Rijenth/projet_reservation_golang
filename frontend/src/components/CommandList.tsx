baf8d4f06523e115aaecc4c0c40208a78b71e869
Rijenth <a_rijenth@laposte.net>
2024-02-10 13:24:52 +0100

    fix: commad list item display order

--- frontend/src/components/CommandList.tsx
+++ frontend/src/components/CommandList.tsx
@@ -14,54 +14,57 @@ export default function CommandList({
 
     return (
         <div className="flex flex-col space-y-4 overflow-y-auto h-full p-4 rounded-lg no-scrollbar">
-            {commands.map((command) => (
-                <div
-                    onClick={() => {
-                        if (selectedCommandId !== Number(command.id))
-                            setSelectedCommandId(Number(command.id));
-                    }}
-                    key={command.id}
-                    className="flex flex-col items-left justify-center bg-white p-4 rounded-lg shadow-md w-96 hover:bg-gray-800 hover:text-white hover:border-2 hover:border-white"
-                >
-                    <h2 className="text-sm font-bold">
-                        {command.attributes.description}{' '}
-                    </h2>
-                    <p className="text-sm font-bold">
-                        Montant : {roundFloatNumber(command.attributes.amount)}{' '}
-                        €
-                    </p>
+            {commands
+                .sort()
+                .reverse()
+                .map((command) => (
+                    <div
+                        onClick={() => {
+                            if (selectedCommandId !== Number(command.id))
+                                setSelectedCommandId(Number(command.id));
+                        }}
+                        key={command.id}
+                        className="flex flex-col items-left justify-center bg-white p-4 rounded-lg shadow-md w-96 hover:bg-gray-800 hover:text-white hover:border-2 hover:border-white"
+                    >
+                        <h2 className="text-sm font-bold">
+                            {command.attributes.description}{' '}
+                        </h2>
+                        <p className="text-sm font-bold">
+                            Montant :{' '}
+                            {roundFloatNumber(command.attributes.amount)} €
+                        </p>
 
-                    {command.attributes.identificationNumber && (
-                        <div
-                            className={`${command.attributes.status === 'ready' ? 'bg-green-500' : 'bg-gray-500'} w-full text-white text-center mt-2 text-black p-2 rounded-lg`}
-                        >
-                            <span className="text-sm font-bold underline">
-                                {command.attributes.status === 'ready'
-                                    ? `Code de retrait : `
-                                    : 'La commande a été retirée'}
-                            </span>
-                            <p className="text-xs">
-                                {command.attributes.status === 'ready'
-                                    ? `${command.attributes.identificationNumber}`
-                                    : ''}
-                            </p>
-                        </div>
-                    )}
-
-                    {selectedCommandId === Number(command.id) && (
-                        <>
-                            <SelectedCommandInfos command={command} />
-
-                            <button
-                                className="bg-gray-500 hover:bg-gray-600 px-4 py-2 text-white rounded mt-2"
-                                onClick={() => setSelectedCommandId(0)}
+                        {command.attributes.identificationNumber && (
+                            <div
+                                className={`${command.attributes.status === 'ready' ? 'bg-green-500' : 'bg-gray-500'} w-full text-white text-center mt-2 text-black p-2 rounded-lg`}
                             >
-                                Fermer
-                            </button>
-                        </>
-                    )}
-                </div>
-            ))}
+                                <span className="text-sm font-bold underline">
+                                    {command.attributes.status === 'ready'
+                                        ? `Code de retrait : `
+                                        : 'La commande a été retirée'}
+                                </span>
+                                <p className="text-xs">
+                                    {command.attributes.status === 'ready'
+                                        ? `${command.attributes.identificationNumber}`
+                                        : ''}
+                                </p>
+                            </div>
+                        )}
+
+                        {selectedCommandId === Number(command.id) && (
+                            <>
+                                <SelectedCommandInfos command={command} />
+
+                                <button
+                                    className="bg-gray-500 hover:bg-gray-600 px-4 py-2 text-white rounded mt-2"
+                                    onClick={() => setSelectedCommandId(0)}
+                                >
+                                    Fermer
+                                </button>
+                            </>
+                        )}
+                    </div>
+                ))}
         </div>
     );
 }
